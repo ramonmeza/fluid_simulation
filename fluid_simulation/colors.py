32850c070aa5b127bece393f9b7a92389c488414
Ramon Meza <ramon_meza@live.com>
2023-10-08 17:23:05 -0400

    Add Green as a color

--- fluid_simulation/colors.py
+++ fluid_simulation/colors.py
@@ -5,3 +5,4 @@ class Colors:
     Black: pygame.Color = pygame.Color(0, 0, 0)
     White: pygame.Color = pygame.Color(255, 255, 255)
     LightBlue: pygame.Color = pygame.Color(173, 216, 230)
+    Green: pygame.Color = pygame.Color(0, 255, 0)
