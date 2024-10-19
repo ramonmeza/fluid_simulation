2ca65a8aedfd091c79701ced506a817705cdbd1c
Ramon Meza <ramon_meza@live.com>
2024-10-19 09:58:44 -0400

    Some updates

--- fluid_simulation/fluid_particle.py
+++ fluid_simulation/fluid_particle.py
@@ -1,3 +1,7 @@
+import math
+from typing import List
+
+
 import pygame
 
 from .colors import Colors
@@ -8,34 +12,49 @@ from .simulation_params import SimulationParams
 
 
 class FluidParticle(GameObject):
-    position: Vector2
-    velocity: Vector2
+    positions: List[Vector2]
+    velocities: List[Vector2]
 
     def __init__(self, position: Vector2) -> None:
         super().__init__()
-        self.position = position
-        self.velocity = Vector2.zero
+        self.positions = [Vector2.zero] * SimulationParams.particle_count
+        self.velocities = [Vector2.zero] * SimulationParams.particle_count
+        
+        particles_per_row = int(math.sqrt(SimulationParams.particle_count))
+        particles_per_col = (SimulationParams.particle_count - 1) / particles_per_row + 1
+        spacing = SimulationParams.particle_radius * 2 + SimulationParams.particle_spacing
+        
+        for i in range(SimulationParams.particle_count):
+            x = (i % particles_per_row - particles_per_row / 2 + 0.5) * spacing
+            y = (i / particles_per_row - particles_per_col / 2 + 0.5) * spacing
+            self.positions[i] = position + Vector2(x, y)
 
     def update(self, dt: float) -> None:
-        self.velocity += Vector2.down * SimulationParams.gravity * dt
-        self.position += self.velocity * dt
-        self.resolve_collisions()
+        for i in range(len(self.positions)):
+            self.velocities[i] += Vector2.down * SimulationParams.gravity * dt
+            self.positions[i] += self.velocities[i] * dt
+            self.resolve_collisions()
 
     def render(self, screen) -> None:
-        pygame.draw.circle(
-            screen, Colors.LightBlue, self.position, SimulationParams.particle_radius
-        )
+        for position in self.positions:
+            pygame.draw.circle(
+                screen, Colors.LightBlue, position, SimulationParams.particle_radius
+            )
 
     def resolve_collisions(self) -> None:
-        half_bounds_size: Vector2 = (
-            SimulationParams.get_bounds() / 2
-            - Vector2.one * SimulationParams.particle_radius
-        )
+        for i in range(SimulationParams.particle_count):
+            # simulation bounding box
+            half_bounds_size: Vector2 = (
+                SimulationParams.get_bounds() / 2
+                - Vector2.one * SimulationParams.particle_radius
+            )
 
-        if abs(self.position.x) > half_bounds_size.x:
-            self.position.x = half_bounds_size.x * sign(self.position.x)
-            self.velocity.x *= -1 * SimulationParams.collision_dampening
+            if abs(self.positions[i].x) > half_bounds_size.x:
+                self.positions[i].x = half_bounds_size.x * sign(self.positions[i].x)
+                self.velocities[i].x *= -1 * SimulationParams.collision_dampening
 
-        if abs(self.position.y) > half_bounds_size.y:
-            self.position.y = half_bounds_size.y * sign(self.position.y)
-            self.velocity.y *= -1 * SimulationParams.collision_dampening
+            if abs(self.positions[i].y) > half_bounds_size.y:
+                self.positions[i].y = half_bounds_size.y * sign(self.positions[i].y)
+                self.velocities[i].y *= -1 * SimulationParams.collision_dampening
+
+            # collisions between particles
--- fluid_simulation/simulation_params.py
+++ fluid_simulation/simulation_params.py
@@ -4,8 +4,11 @@ from .vector2 import Vector2
 
 
 class SimulationParams:
+    particle_count: int = 10
+    particle_spacing: float = 10.0
+
     particle_radius: float = 10.0
-    gravity: float = 0.0
+    gravity: float = 9.8
     bounds_size: Vector2 = Vector2(500, 500)
     collision_dampening: float = 1.0
 
